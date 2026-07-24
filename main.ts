06a7346819d6654b0125cef9a0443bc796c193cb
youngsikhcoder <vihaan.jabbal@gmail.com>
2026-07-23 19:31:50 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,13 @@
+let Number1 = 0
+let Number2 = 0
 input.onButtonPressed(Button.A, function () {
-    Ship.move(-1)
-})
-input.onButtonPressed(Button.AB, function () {
-    Shoot = game.createSprite(Ship.get(LedSpriteProperty.X), Ship.get(LedSpriteProperty.Y))
-    Shoot.change(LedSpriteProperty.Blink, 80)
-    for (let index = 0; index < 4; index++) {
-        Shoot.change(LedSpriteProperty.Y, -1)
-        basic.pause(150)
-        if (Shoot.isTouching(Enemy)) {
-            game.addScore(1)
-            Enemy.delete()
-            Shoot.delete()
-        }
-    }
-    Shoot.delete()
+    Number1 = 0
+    basic.showNumber(Number1)
 })
 input.onButtonPressed(Button.B, function () {
-    Ship.move(1)
+    Number2 += 1
+    basic.showNumber(Number2)
 })
-let Enemy: game.LedSprite = null
-let Shoot: game.LedSprite = null
-let Ship: game.LedSprite = null
-Ship = game.createSprite(2, 4)
-game.setScore(0)
-basic.forever(function () {
-    Enemy = game.createSprite(randint(0, 4), 0)
-    Enemy.set(LedSpriteProperty.Brightness, 150)
-    basic.pause(100)
-    Enemy.turn(Direction.Right, 90)
-    for (let index = 0; index < 4; index++) {
-        Enemy.move(1)
-        basic.pause(500)
-    }
-    if (Enemy.isTouching(Ship)) {
-        game.gameOver()
-    }
-    if (Enemy.isTouchingEdge()) {
-        Enemy.delete()
-    }
-    basic.pause(100)
+input.onGesture(Gesture.Shake, function () {
+    basic.pause(Number1 + Number2)
 })
